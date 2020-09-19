908562c7e8103a3da0a5c5e9d62ce4353a637064
Shivam Tandon <70521608+shivamtandon1999@users.noreply.github.com>
2020-09-19 15:23:59 +0530

    added scroll up btn

--- about/script.js
+++ about/script.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
   $(window).scroll(function () {
+    // sticky navbar on scroll script
     if (this.scrollY > 20) {
       $(".navbar").addClass("sticky");
     } else {
       $(".navbar").removeClass("sticky");
     }
-    if (this.scroll > 500) {
+
+    // scroll-up button show/hide script
+    if (this.scrollY > 200) {
       $(".scroll-up-btn").addClass("show");
     } else {
       $(".scroll-up-btn").removeClass("show");
@@ -14,7 +17,14 @@ $(document).ready(function () {
 
   //slide up script
   $(".scroll-up-btn").click(function () {
-    $(html).animate({ scrollTop: 0 });
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
+  });
+
+  $(".navbar .menu li a").click(function () {
+    // applying again smooth scroll on menu items click
+    $("html").css("scrollBehavior", "smooth");
   });
 
   // toggle menu/navbar script
